610133429ee29a7a96498152b8027e1b7f5d8d7b
Michael Felix <mikemh@uri.edu>
2023-11-27 13:23:18 -0500

    outline heuristic

--- src/Othello/Headers/AiPlayer.hpp
+++ src/Othello/Headers/AiPlayer.hpp
@@ -8,8 +8,7 @@
 #ifndef AiPlayer_hpp
 #define AiPlayer_hpp
 
-#include "Player.hpp"
-#include "Tile.hpp"
+#include "TurnLogic.hpp" // turnlogic includes board, player, tile and more
 
 
 namespace othello {
@@ -24,11 +23,28 @@ struct MoveScore {
 
 class AiPlayer : public Player {
 private:
+    static const int MOBILITY_WEIGHT_, STABILITY_WEIGHT_, CORNER_WEIGHT_;
+    
+    /// Finds the number of possible moves based on the given boardstate
+    void calculateMobility_(std::vector<std::vector<std::shared_ptr<Tile>>>& hypotheticalBoard, MoveScore& score);
+    
+    /// 
+    void calculateStability_(std::vector<std::vector<std::shared_ptr<Tile>>>& hypotheticalBoard, MoveScore& score);
+    
+    /// Adds the amount of corners controlled by the AI to the score
+    void calculateCornerControl_(std::vector<std::vector<std::shared_ptr<Tile>>>& hypotheticalBoard, MoveScore& score);
+    
+    /// Calculates the score multiplied by all the AI's weights for the given boardstate
+    /// @param score the score to multiply by weights
+    void computeTotalScore_(std::vector<std::vector<std::shared_ptr<Tile>>>& hypotheticalBoard, MoveScore& score);
     
 public:
     AiPlayer(RGBColor myColor);
     AiPlayer(RGBColor myColor, std::string name);
-        
+    
+    /// Calculate the score for the AI's position based on a given boardstate
+    MoveScore evaluateBoardState(std::vector<std::vector<std::shared_ptr<Tile>>>& hypotheticalBoard);
+    
     //disabled constructors & operators
     AiPlayer() = delete;
     AiPlayer(const AiPlayer& obj) = delete;    // copy
