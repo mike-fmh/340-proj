5954ed216d5145344e1853e191423735bd7bfacc
Michael Felix <mikemh@uri.edu>
2023-11-30 09:31:21 -0500

    some cleanup

--- src/Othello/main.cpp
+++ src/Othello/main.cpp
@@ -138,9 +138,10 @@ void mySpecialKeyUpHandler(int key, int x, int y);
 void myTimerFunc(int val);
 void applicationInit();
 
+void startTurn(shared_ptr<Player>& whoseTurn);
 void addGamePiece(TilePoint location, shared_ptr<Player> whose, shared_ptr<Board> theBoard, bool addObj);
 unsigned int evalGamestateScore(shared_ptr<Player>& AIplayer, shared_ptr<GameState>& hypotheticalGamestate);
-TilePoint bestMoveHeuristic(shared_ptr<Player>& AIplayer, vector<shared_ptr<Tile>> possibleMoves);
+unsigned int bestMoveHeuristic(shared_ptr<Player>& AIplayer, vector<shared_ptr<Tile>> possibleMoves);
 
 //--------------------------------------
 #if 0
@@ -215,12 +216,67 @@ float Board::drawInPixelScale;
 const GLfloat* textColor = TEXT_COLOR[0];
 const GLfloat* bgndColor = BGND_COLOR[0];
 
+RGBColor WHITE = RGBColor{1, 1, 1};
+RGBColor BLACK = RGBColor{0, 0, 0};
 
 #if 0
 #pragma mark -
 #pragma mark Callback functions
 #endif
 
+
+void startTurn(shared_ptr<Player>& whoseTurn) {
+    if (whoseTurn->getMyColor().isEqualTo(WHITE)) {
+        /// White's turn
+        
+        // it's not black's turn, so clear their moves
+        if (blackPlayableTiles.size() > 0) {
+            for (auto tile: blackPlayableTiles) {
+                tile->setColor(DEFAULT_TILE_COLOR);
+            }
+            blackPlayableTiles.clear();
+        }
+        
+        // populate white's moves if not already done
+        if (whitePlayableTiles.size() == 0) {
+            gameState->getPlayableTiles(playerWhite, whitePlayableTiles);
+            if (whitePlayableTiles.size() == 0) { // if white has no valid moves
+                currentTurn = 0; // this equates to passing the turn to black (handled in the timer function)
+                gameState->passTurn(playerBlack);
+            }
+        }
+        
+        // display current possible moves
+        for (auto tile: whitePlayableTiles) {
+            tile->setColor(0.8, 1, 1);
+        }
+    } else if (whoseTurn->getMyColor().isEqualTo(BLACK)) {
+        /// Black's turn
+        
+        // it's not white's turn, so clear their moves
+        if (whitePlayableTiles.size() > 0) {
+            for (auto tile: whitePlayableTiles) {
+                tile->setColor(DEFAULT_TILE_COLOR);
+            }
+            whitePlayableTiles.clear();
+        }
+        
+        // populate black's moves if not already done
+        if (blackPlayableTiles.size() == 0) {
+            gameState->getPlayableTiles(playerBlack, blackPlayableTiles);
+            if (blackPlayableTiles.size() == 0) { // if black has no valid moves
+                currentTurn = 1; // pass the turn to white (handled in the timer function)
+                gameState->passTurn(playerWhite);
+                cur_ai_turn_wait = 0;
+            }
+        }
+        
+         // display current possible moves
+         for (auto tile: blackPlayableTiles) {
+         tile->setColor(1, 0.8, 1);
+         }
+    }
+}
 
 void addGamePiece(TilePoint location, shared_ptr<Player> whose, shared_ptr<Board> theBoard, bool addObj) {
     shared_ptr<Disc> thisDisc = make_shared<Disc>(location, whose->getMyColor());
@@ -267,15 +323,12 @@ unsigned int evalGamestateScore(shared_ptr<Player>& AIplayer, shared_ptr<GameSta
 }
 
 
-TilePoint bestMoveHeuristic(shared_ptr<Player>& AIplayer, vector<shared_ptr<Tile>> possibleMoves) {
-    TilePoint bestMoveLoc;
-    
-    RGBColor WHITE = RGBColor{1, 1, 1};
-    RGBColor BLACK = RGBColor{0, 0, 0};
-    
+unsigned int bestMoveHeuristic(shared_ptr<Player>& AIplayer, vector<shared_ptr<Tile>> possibleMoves) {
+    unsigned int bestMoveInd = 0;
     unsigned int bestMoveScore = 0;
     unsigned int curMoveScore = 0;
-    for (shared_ptr<Tile> thisMove : possibleMoves) {
+    for (unsigned int i = 0; i < possibleMoves.size(); i++) {
+        shared_ptr<Tile> thisMove = possibleMoves[i];
         shared_ptr<Player> tempWhite = make_shared<Player>(WHITE, "white");
         shared_ptr<Player> tempBlack = make_shared<Player>(BLACK, "black");
         shared_ptr<Player> tempNull = make_shared<Player>(RGBColor{-1, -1, -1}, "null");
@@ -308,12 +361,12 @@ TilePoint bestMoveHeuristic(shared_ptr<Player>& AIplayer, vector<shared_ptr<Tile
         */
         
         if (curMoveScore > bestMoveScore) {
-            bestMoveLoc = thisMoveLoc;
+            bestMoveInd = i;
             bestMoveScore = curMoveScore;
         }
     
     }
-    return bestMoveLoc;
+    return bestMoveInd;
 }
 
 void myDisplayFunc(void)
@@ -495,50 +548,18 @@ void myTimerFunc(int value)
     // do stuff
     
     if (currentTurn) { // white's turn
-        // it's not black's turn, so clear their moves
-        if (blackPlayableTiles.size() > 0) {
-            for (auto tile: blackPlayableTiles) {
-                tile->setColor(DEFAULT_TILE_COLOR);
-            }
-            blackPlayableTiles.clear();
-        }
+        startTurn(playerWhite);
         
-        // populate white's moves if not already done
-        if (whitePlayableTiles.size() == 0)
-            gameState->getPlayableTiles(playerWhite, whitePlayableTiles);
-        
-        
-        // display current possible moves
-        for (auto tile: whitePlayableTiles) {
-            tile->setColor(0.8, 1, 1);
-        }
+        // white's turn logic is handled in the mouse function
         
     } else {
-        // it's not white's turn, so clear their moves
-        if (whitePlayableTiles.size() > 0) {
-            for (auto tile: whitePlayableTiles) {
-                tile->setColor(DEFAULT_TILE_COLOR);
-            }
-            whitePlayableTiles.clear();
-        }
+        startTurn(playerBlack);
         
-        // populate black's moves if not already done
-        if (blackPlayableTiles.size() == 0)
-            gameState->getPlayableTiles(playerBlack, blackPlayableTiles);
-        
-        
-         // display current possible moves
-         for (auto tile: blackPlayableTiles) {
-         tile->setColor(1, 0.8, 1);
-         }
-         
-        
+        // black's (AI) turn logic
         if (cur_ai_turn_wait >= SECS_BETWEEN_AI_MOVES) {
             // compute black's best move and play it
-            cout << "game pices: " << (int)gameBoard->getAllPieces().size() << endl;
-            
-            TilePoint bestMoveLoc = bestMoveHeuristic(playerBlack, blackPlayableTiles);
-          
+            unsigned int bestMoveIndex = bestMoveHeuristic(playerBlack, blackPlayableTiles);
+            TilePoint bestMoveLoc = blackPlayableTiles[bestMoveIndex]->getPos();
             shared_ptr<Tile> bestMove = gameBoard->getBoardTile(bestMoveLoc);
             shared_ptr<Disc> newPiece = gameState->placePiece(playerBlack, bestMove);
             allObjects.push_back(newPiece);
