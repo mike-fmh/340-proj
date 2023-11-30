1f82075d4f0b916d7759ac7f150335fc2c82c06a
Michael Felix <mikemh@uri.edu>
2023-11-30 10:35:55 -0500

    Update Disc.cpp

--- src/Othello/Source/Disc.cpp
+++ src/Othello/Source/Disc.cpp
@@ -66,8 +66,7 @@ void Disc::update(float dt) {
     if (switchColorAfter_ > -1) {
         curSwitchTimer_ += dt;
         if (curSwitchTimer_ >= switchColorAfter_) {
-            color_ = colorToSwitch_;
-            switchColorAfter_ = -1;
+            applyPendingColorChange();
         }
     } else {
         colorToSwitch_ = RGBColor{-1, -1, -1};
